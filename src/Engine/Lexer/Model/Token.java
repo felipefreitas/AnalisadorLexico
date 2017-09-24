238e0dc972ad007bc629a9ef9e92a78a16f9469f
Felipe Freitas <felipesafreitas@gmail.com>
2017-09-24 18:10:54 -0300

    added token model

---
+++ src/Engine/Lexer/Model/Token.java
@@ -0,0 +1,12 @@
+package Engine.Lexer.Model;
+
+public class Token {
+
+    public TokenType Type;
+    public String Content;
+
+    public Token(TokenType tokenType, String content) {
+        Type = tokenType;
+        Content = content;
+    }
+}
